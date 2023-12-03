8396d2e964299c35bf3d3be148b1f579e4831ed5
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-12-03 20:28:50 +0530

    Update server.py

--- utils/server.py
+++ utils/server.py
@@ -24,5 +24,5 @@
 #async def route():
    # return "<h1>Check <a href='https://github.com/arun017s/AutoDelete'>AutoDelete</a></h1>"
 
-if __name__ == "__main__":     
+#if __name__ == "__main__":     
  #  app.run()
